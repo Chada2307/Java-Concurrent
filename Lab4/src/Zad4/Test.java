6dee52d5dff793cbe5d486fba2aa8ce94c8f0778
Adrian Bielenik <adrian.bielenik@gmail.com>
2025-04-06 18:33:14 +0200

    update06-04

--- Lab4/src/Zad4/Test.java
+++ Lab4/src/Zad4/Test.java
@@ -6,27 +6,27 @@ public class Test {
 
     public static void main(String[] args) {
 
-        final int lw = 7; // liczba watkow
+        final int lw = 500; // liczba watkow
         Random r = new Random();
         Watek[] watki = new Watek[lw];
         int n = 500_000_000;
-        int[] tabi = new int[n];
-      //  double[] tabd = new double[n];
+        //int[] tabi = new int[n];
+        double[] tabd = new double[n];
 
 
-        for (int i = 0; i < n; i++) {
-            tabi[i] = r.nextInt(1_000_000_000);
-        }
-     //   for (int i = 0; i < n; i++) {
-       //     tabd[i] = r.nextDouble(1_000_000_000);
+       // for (int i = 0; i < n; i++) {
+            // tabi[i] = r.nextInt(1_000_000_000);
        // }
+        for (int i = 0; i < n; i++) {
+            tabd[i] = r.nextDouble(1_000_000_000);
+        }
         System.out.println("Koniec losowania-");
         long startTime  = System.nanoTime();
 
         for (int i = 0; i < lw; i++) {
             int od = i * (n / lw);
             int doo = (i + 1) * (n / lw);
-            watki[i] = new Watek(tabi, od, doo);
+            watki[i] = new Watek(tabd, od, doo);
             watki[i].start();
 
         }
@@ -49,8 +49,8 @@ public class Test {
 
 
         for (int i = 0; i < 1; i++) {
-            int mini = 0;
-            int maxx = 0;
+            double mini = 0;
+            double maxx = 0;
             for (int j = 0; j < 4; j++) {
                 if (Watek.minimal[j] < mini) {
                     mini = Watek.minimal[j];
@@ -60,8 +60,6 @@ public class Test {
                 }
 
             }
-
-
         }
         System.out.println("Koniec programu: "+ (System.nanoTime()-startTime)/1_000_000+" s");
     }
